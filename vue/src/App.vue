0e79805b68fb9d7fd0a51d65669a0a81df1420fb
Nathaniel Roumani <nroumani.g@gmail.com>
2024-04-05 13:54:58 -0400

    added minor styling to App.vue & added logo to assets

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="capstone-app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }" class="home-link">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style>
+.home-link {
+  text-decoration: none;
+  color: #708090;
+}
+</style>
