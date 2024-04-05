f209d98c77076f9bca6959219a199e51bceca739
Ben Wolfer <benwwwiv@gmail.com>
2024-04-05 17:08:03 -0400

    created the landmarks table in the schema, no data yet, and the city id is just defaulted to one. DO A ./create.sh

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS landmarks;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +10,20 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE landmarks (
+	landmark_id SERIAL PRIMARY KEY,
+	name varchar(64) NOT NULL,
+	description varchar(500) DEFAULT 'No Description Provided',
+	weekday_open time DEFAULT '00:00:00',
+	weekday_close time DEFAULT '00:00:00',
+	weekend_open time DEFAULT '00:00:00',
+	weekend_close time DEFAULT '00:00:00',
+	category varchar(35) NOT NULL,
+	city_id int DEFAULT 1,
+	like_count int,
+	dislike_count int
+);
+
 
 COMMIT TRANSACTION;
