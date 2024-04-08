72ed1ff9f8bae89217eb0d5d456edd1b9beedf14
Ben Wolfer <benwwwiv@gmail.com>
2024-04-08 16:53:54 -0400

    We added filter functionality for both categories and normal search

--- vue/src/views/LandmarksView.vue
+++ vue/src/views/LandmarksView.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
         <h1>Landmarks</h1>
-        <input type="search">
+        <input type="text" v-model="searchQuery" placeholder="Search...">
+        <!-- <input type="text" v-model="categoryQuery" placeholder="Category..."> -->
+        <select v-model="selectedCategory">
+            <option value="">All Categories</option>
+            <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+        </select>
         <section class="landmark-list">
-            <LandmarkSimplified v-for="landmark in landmarks" :key="landmark.id" :landmark="landmark" />
+            <LandmarkSimplified v-for="landmark in filteredLandmarks" :key="landmark.id" :landmark="landmark" />
         </section>
     </div>
 </template>
@@ -13,6 +18,13 @@ import LandmarkService from '../services/LandmarkService';
 import LandmarkSimplified from '../components/LandmarkSimplified.vue';
 
 export default {
+    data() {
+        return {
+            searchQuery: '',
+            selectedCategory: '',
+            categories: ['Public Space', 'Attraction', 'Museum', 'Landmark', 'Botanical Garden', 'Entertainment', 'Observatory'],
+        };
+    },
     name: "LandmarksView",
     components: {
         LandmarkSimplified,
@@ -20,7 +32,12 @@ export default {
     computed: {
         landmarks() {
             return this.$store.state.landmarks;
-        }
+        },
+        filteredLandmarks() {
+            return this.landmarks.filter(landmark => {
+                return landmark.name.toLowerCase().includes(this.searchQuery.toLowerCase()) && (this.selectedCategory === '' || landmark.category.toLowerCase() === this.selectedCategory.toLowerCase());
+            });
+        },
     },
     created() {
         const landmarks = LandmarkService.getLandmarks()
