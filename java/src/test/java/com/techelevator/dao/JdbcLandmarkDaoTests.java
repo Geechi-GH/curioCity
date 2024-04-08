bedae6c8321415b326c54a52046d53cb5b1ae39b
Dennis Smith <dennisksmith21@gmail.com>
2024-04-08 13:08:01 -0400

    Created tests for our get all landmarks endpoint

---
+++ java/src/test/java/com/techelevator/dao/JdbcLandmarkDaoTests.java
@@ -0,0 +1,54 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Landmark;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcLandmarkDaoTests extends BaseDaoTests{
+
+    private static final Landmark TEST_LANDMARK_1 = new Landmark(1,"Test Landmark 1", "Test Description 1", new Time(9,0,0), new Time(17,0,0), new Time(10,0,0), new Time(16,0,0), "Test Category 1", 1, 0, 0);
+    private static final Landmark TEST_LANDMARK_2 = new Landmark(2,"Test Landmark 2", "Test Description 2", new Time(9,0,0), new Time(17,0,0), new Time(10,0,0), new Time(16,0,0), "Test Category 1", 1, 0, 0);
+    private static final Landmark TEST_LANDMARK_3 = new Landmark(3,"Test Landmark 3", "Test Description 3", new Time(10,30,0), new Time(18,30,0), new Time(12,0,0), new Time(17,0,0), "Test Category 2", 1, 0, 0);
+
+    private JdbcLandmarkDao sut;
+@Before
+    public void setup() {
+    sut = new JdbcLandmarkDao(dataSource);
+}
+
+@Test
+    public void get_all_landmarks_works() {
+    //arrange
+    List <Landmark> testList = new ArrayList<>();
+    //act
+    testList = sut.getAllLandmarks();
+
+    //assert
+    Assert.assertEquals(3,testList.size());
+    assertLandmarksEqual(testList.get(0),TEST_LANDMARK_1);
+    assertLandmarksEqual(testList.get(1), TEST_LANDMARK_2);
+}
+
+public void assertLandmarksEqual(Landmark actual, Landmark expected) {
+    Assert.assertEquals(expected.getId(), actual.getId());
+    Assert.assertEquals(expected.getName(), actual.getName());
+    Assert.assertEquals(expected.getDescription(), actual.getDescription());
+    Assert.assertEquals(expected.getWeekdayOpen(), actual.getWeekdayOpen());
+    Assert.assertEquals(expected.getWeekdayClose(), actual.getWeekdayClose());
+    Assert.assertEquals(expected.getWeekendOpen(), actual.getWeekendOpen());
+    Assert.assertEquals(expected.getWeekendClose(), actual.getWeekendClose());
+    Assert.assertEquals(expected.getCategory(), actual.getCategory());
+    Assert.assertEquals(expected.getCityID(), actual.getCityID());
+    Assert.assertEquals(expected.getLikeCount(), actual.getLikeCount());
+    Assert.assertEquals(expected.getDislikeCount(), actual.getDislikeCount());
+
+}
+
+}
+
+
--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -4,4 +4,11 @@ INSERT INTO users (username,password_hash,role) VALUES ('user1','user1','ROLE_US
 INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
 
+INSERT INTO landmarks (landmark_id, name, description, weekday_open, weekday_close, weekend_open, weekend_close, category, city_id, like_count, dislike_count)
+VALUES
+(1, 'Test Landmark 1', 'Test Description 1', '09:00:00', '17:00:00', '10:00:00', '16:00:00', 'Test Category 1', 1, 0, 0),
+(2, 'Test Landmark 2', 'Test Description 2', '09:00:00', '17:00:00', '10:00:00', '16:00:00', 'Test Category 1', 1, 0, 0),
+(3, 'Test Landmark 3', 'Test Description 3', '10:30:00', '18:30:00', '12:00:00', '17:00:00', 'Test Category 2', 1, 0, 0);
+
+
 COMMIT TRANSACTION;
