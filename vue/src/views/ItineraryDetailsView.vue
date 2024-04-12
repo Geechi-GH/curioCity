408705e722f5e4b0986f78f7f1b81e49b8ed449c
Joseph Fredrickson <joeyfredrickson@gmail.com>
2024-04-12 18:28:20 -0400

    it works

--- vue/src/views/ItineraryDetailsView.vue
+++ vue/src/views/ItineraryDetailsView.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
-        <p>YOUR LANDMARKS</p>
-        <section v-for="landmark in populateLandmarks" :key="landmark.id">
+        <draggable v-model="landmarksToVisit" item-key="id" @end="saveItinerary">
+            <template #item="{ element }">
+                <LandmarksMinus :landmark="element" :itinerary="itinerary" @remove-landmark="removeLandmark">
+                </LandmarksMinus>
+            </template>
+        </draggable>
+
+        <p>_________YOUR LANDMARKS___________</p>
+        <!-- <section v-for="landmark in populateLandmarks" :key="landmark.id">
             <LandmarksMinus :landmark="landmark" v-if="itinerary.landmarksArray.includes(landmark.id)"
                 v-bind:itinerary="itinerary" @remove-landmark="removeLandmark">
             </LandmarksMinus>
-        </section>
-        <p>LANDMARKS TO ADD</p>
-        <section v-for="landmark in populateLandmarks" :key="landmark.id">
-            <LandmarksPlus :landmark="landmark" v-if="!itinerary.landmarksArray.includes(landmark.id)"
-                v-bind:itinerary="itinerary" @add-landmark="addLandmark">
+        </section> -->
+        <p>_________LANDMARKS TO ADD___________</p>
+        <section v-for="landmark in availableLandmarks" :key="landmark.id">
+            <LandmarksPlus :landmark="landmark" v-bind:itinerary="itinerary" @add-landmark="addLandmark">
             </LandmarksPlus>
         </section>
     </div>
@@ -20,52 +26,74 @@ import ItineraryService from '../services/ItineraryService';
 import LandmarkService from '../services/LandmarkService';
 import LandmarksPlus from '../components/LandmarksPlus.vue';
 import LandmarksMinus from '../components/LandmarksMinus.vue';
+import draggable from 'vuedraggable';
+
+
 export default {
     components: {
         LandmarksPlus,
-        LandmarksMinus
+        LandmarksMinus,
+        draggable
     },
     data() {
         return {
+            myArray: [
+                { id: 1, name: "test" },
+                { id: 2, name: "test2" }
+            ],
+            landmarksToVisit: [],
             itinerary: {
                 landmarksArray: [],
             }
         }
     },
     methods: {
+        saveItinerary() {
+            console.log("saving...")
+            this.itinerary.landmarksArray = this.landmarksToVisit.map(landmark => landmark.id);
+            console.log(this.itinerary.landmarksArray);
+
+            console.log(this.landmarksToVisit);
+            ItineraryService.flushAndFill(this.itinerary, this.itinerary.itineraryId)
+                .then(response => {
+                    console.log(response.data);
+                    this.itinerary = response.data;
+                    // this.landmarksToVisit = this.$store.state.landmarks
+                    //     .filter(lm => this.itinerary.landmarksArray.includes(lm.id));
+                });
+        },
         getItinerary(id) {
             ItineraryService.getItineraryById(id).then(response => {
                 this.itinerary = response.data;
-                this.itinerary.landmarksArray = response.data.landmarksArray;
+                // this.itinerary.landmarksArray = response.data.landmarksArray;
+                this.landmarksToVisit = this.itinerary.landmarksArray
+                    .map(id => this.$store.state.landmarks.find(landmark => landmark.id === id));
             })
         },
         addLandmark(landmark) {
-            this.itinerary.landmarksArray.push(landmark.id);
-            ItineraryService.addingLandmarkToItinerary(this.itinerary, this.itinerary.itineraryId)
-                .then(response => {
-                    this.itinerary = response.data;
-                });
-            ItineraryService.getItineraryById(this.itinerary.itineraryId).then(response => {
-                this.itinerary.landmarksArray = response.data.landmarksArray
-            })
+            // this.itinerary.landmarksArray.push(landmark.id);
+            this.landmarksToVisit.push(landmark);
+            this.saveItinerary()
+            // i don't think this is needed
+            // ItineraryService.getItineraryById(this.itinerary.itineraryId).then(response => {
+            //     this.itinerary.landmarksArray = response.data.landmarksArray
+            // })
         },
         removeLandmark(landmark) {
-            this.itinerary.landmarksArray.splice(this.itinerary.landmarksArray.indexOf(landmark.id), 1);
-            ItineraryService.flushAndFill(this.itinerary, this.itinerary.itineraryId)
-                .then(response => {
-                    this.itinerary = response.data;
-                });
-            ItineraryService.getItineraryById(this.itinerary.itineraryId).then(response => {
-                this.itinerary.landmarksArray = response.data.landmarksArray
-            })
+            this.landmarksToVisit = this.landmarksToVisit
+                .filter(lm => lm.id !== landmark.id);
+            this.saveItinerary();
+            // this.itinerary.landmarksArray = this.itinerary.landmarksArray
+            //     .filter(id => id !== landmark.id);
+
         }
     },
     created() {
         const landmarks = LandmarkService.getLandmarks()
             .then(response => {
                 this.$store.commit('SET_LANDMARKS', response.data);
+                this.getItinerary(this.$route.params.itineraryId)
             });
-        this.getItinerary(this.$route.params.itineraryId)
     },
     computed: {
         landmarksArray() {
@@ -73,9 +101,16 @@ export default {
         },
         populateLandmarks() {
             return this.$store.state.landmarks
+        },
+        availableLandmarks() {
+            const selectedLandmarkIds = this.landmarksToVisit.map(landmark => landmark.id);
+            return this.$store.state.landmarks
+                .filter(lm => !selectedLandmarkIds.includes(lm.id));
         }
     }
 }
 </script>
+
+
 
 <style scoped></style>
