5eabda8367fb3255c4f9ef484ee7865f7eadad59
john loper <johnloper@gmail.com>
2024-04-15 08:51:34 -0400

    doing a morning push styled home page still needs complete

--- vue/src/components/ItinerariesSimplified.vue
+++ vue/src/components/ItinerariesSimplified.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="itinerary-card">
-        <h1>{{ itinerary.title }}</h1>
-        <p>{{ itinerary.dateOfTravel }}</p>
+        <h1 class="title">{{ itinerary.title }}</h1>
+        <p class="date">{{ itinerary.dateOfTravel }}</p>
     </div>
 </template>
 
@@ -16,4 +16,31 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.title {
+    font-size: 1.5em;
+    font-weight: bold;
+    text-align: center !important;
+    margin-bottom: 15px;
+    color: #fffff0;
+    justify-content: center;
+    font-family: serif;
+    text-decoration: none;
+    text-decoration-line: underline;
+    text-decoration-style: unset;
+    text-decoration-color: #292929;
+}
+
+.date {
+    font-size: 1em;
+    text-align: center !important;
+    margin-bottom: 15px;
+    color: #d7b740;
+    justify-content: center;
+    font-family: serif;
+    text-decoration: none;
+    text-decoration-line: underline;
+    text-decoration-style: unset;
+    text-decoration-color: #292929;
+}
+</style>
